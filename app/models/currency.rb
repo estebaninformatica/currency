246480ea8c6945ffe078f0bed66cd6e37f1c41f0
Your Name <you@example.com>
2015-05-07 10:20:34 -0300

    Currency asociaciones

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,4 @@
 class Currency < ActiveRecord::Base
-	has_many :all_changes, class_name: 'Change' 
+	has_many :all_changes, class_name: 'Change', foreign_key: 'currency_from_id' 
 	validates :name, :description, :abbreviation, presence: true 
 end
