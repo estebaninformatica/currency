b2274e6df87e4ec220b4668623f52ce2f46c55e8
Your Name <you@example.com>
2015-05-07 14:37:29 -0300

    Creacion nuevo controlador

---
+++ app/controllers/type_changes_controller.rb
@@ -0,0 +1,19 @@
+class TypeChangesController < ApplicationController
+  before_action :set_type_change, only: :show 
+  def index
+    @typeChanges=TypeChange.all
+  end
+
+  def show
+  end
+
+  def new
+    @typeChange=TypeChange.new
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_type_change
+      @typeChange = TypeChange.find(params[:id])
+    end
+end
---
+++ app/helpers/type_changes_helper.rb
@@ -0,0 +1,2 @@
+module TypeChangesHelper
+end
