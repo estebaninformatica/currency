13f7bd8fc6ab22575436510e0686bc08fa8922fe
Your Name <you@example.com>
2015-05-07 14:35:54 -0300

    Orden de visualizacion de los campos

--- app/models/type_change.rb
+++ app/models/type_change.rb
@@ -2,16 +2,17 @@ class TypeChange < ActiveRecord::Base
 	belongs_to :change
 	validates :change_id ,:name, :start_dt, :amount, presence: true
 
-#This method creates the new  "TypeChange" and get the last for complete the end_dt
-  def generate
-    lastTyChan=lastTypeChange
-    lastTyChan.nil?
-    lastTyChan.end_dt=Time.now
-    lastTyChan.save
-  end
+#This method get the last TypeChange and completed the end_dt
+def generate
+	lastTyChan=lastTypeChange
+	if  not lastTyChan.nil?
+		lastTyChan.end_dt=Time.now
+		lastTyChan.save
+	end
+end
 
-  def lastTypeChange
-    TypeChange.where("currency_from = ? AND currency_to = ? ", 1, 3).order(start_dt: :desc).take
-  end
+def lastTypeChange
+	TypeChange.where("change_id = ? AND start_dt < ?" , change_id, start_dt ).order(start_dt: :desc).take
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   resources :currencies
-
+  resources :type_changes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'currencies#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
