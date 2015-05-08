9f4db30f8c084b4c13a03d5795a4633b0f544890
Your Name <you@example.com>
2015-05-08 13:04:38 -0300

    Show and edit type change

--- app/controllers/type_changes_controller.rb
+++ app/controllers/type_changes_controller.rb
@@ -1,5 +1,5 @@
 class TypeChangesController < ApplicationController
-  before_action :set_type_change, only: :show 
+  before_action :set_type_change, only: [:show , :edit]
 
   def index
     @typeChanges=TypeChange.all
@@ -13,14 +13,32 @@ class TypeChangesController < ApplicationController
     @currencies=Currency.all
   end
 
+  def edit
+    @currencies=Currency.all
+  end
+
   def create
-    params[:currency_from]
-  
+    @typeChange=TypeChange.new(type_changes_params)
+    respond_to do |format|
+      if @typeChange.save
+        format.html { redirect_to @typeChange, notice: 'Type change was successfully created.' }
+        #format.json { render :show, status: :created, location: @currency }
+      else
+        format.html { render :new }
+        #format.json { render json: @currency.errors, status: :unprocessable_entity }
+      end
+    end
+    
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_type_change
       @typeChange = TypeChange.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def type_changes_params
+      params.require(:type_change).permit(:currency_from_id, :currency_to_id , :name, :start_dt, :end_dt,:amount)
     end
 end
--- app/models/change.rb
+++ app/models/change.rb
@@ -3,4 +3,18 @@ class Change < ActiveRecord::Base
   belongs_to :currency_from, class_name: "Currency" 
   has_many :type_changes
 	validates :currency_from, :currency_to, :start_dt, :end_dt, presence: true
+  validate :validate_distinct_currency, :validate_exist
+
+  def validate_distinct_currency
+    if currency_from_id == currency_to_id
+      errors.add(:currency_from_id, "can't be equal to currency_to_id")
+    end
+  end
+
+  def validate_exist
+    if not Change.where(currency_from_id: currency_from_id , currency_to_id: currency_to_id).empty?
+      errors.add(:id, "This chage exist")
+    end
+  end
+
 end
