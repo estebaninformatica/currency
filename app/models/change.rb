cf18de2af69063f4c5ada7279c1a3e7b8a3db0fc
Your Name <you@example.com>
2015-05-18 11:03:24 -0300

    get change for a date

--- app/models/change.rb
+++ app/models/change.rb
@@ -30,8 +30,13 @@ class Change < ActiveRecord::Base
     self.change_historicals.last
   end
 
+  #method for calendar
   def current_for_date(date)
-    self.change_historicals.reverse.detect {|n| Date.parse(n.start_dt.to_s) <= Date.parse(date)}
+    changes_for_date=[]
+    curr_for_date=self.change_historicals.reverse.detect {|n| Date.parse(n.start_dt.to_s) <= Date.parse(date)}
+    changes_for_date << curr_for_date
+    changes_for_date << self.change_historicals.detect {|chan| chan.start_dt = curr_for_date.end_dt } unless curr_for_date.end_dt.nil?
+    changes_for_date
   end
 
 end
